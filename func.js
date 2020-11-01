ec0afaa8704c1d727af447798a44ec6dab0a6abd
WTF78 <50106339+WTF78@users.noreply.github.com>
2020-11-01 02:01:56 +0200

    Delete func.js

--- func.js
+++
@@ -1,9 +0,0 @@
-async function buttonClick(locator){
-    await page.waitForXPath(locator);
-    const buttonWrite = await page.$x(locator);
-    await buttonWrite[0].click({delay:20});
-}
-
-module.exports ={
-    buttonClick
-}
