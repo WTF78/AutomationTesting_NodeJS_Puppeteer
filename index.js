59064c3c70325272e195fa9b2b9642c864078cbc
WTF78 <oleg.sock@gmail.com>
2020-11-04 00:22:10 +0200

    Last commit I hope)

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const puppeteer = require('puppeteer');
+const config = require('./config.json')
 
-(async () => {
+
+function test(log, pass) {
+    
+ (async () => {
 
     /* Open browser + login ukr.net */
     const browser = await puppeteer.launch({headless: false, defaultViewport: null});
     const page = await browser.newPage();
 
-    const mailName = 'oleg.sock@ukr.net';
     const buttonWriteLetter = '//button[text()=\'Написати листа\']';
     const emailField = '//input[@name=\'toFieldInput\']';
     const emailTheme = '//input[@name=\'subject\']';
 
     await page.goto('https://accounts.ukr.net',{waitUntil:'networkidle0'});
-    await page.type('#id-l',mailName,{delay: 20});
-    await page.type('#id-p','Wtf14412',{delay: 20});
+    await page.type('#id-l',log,{delay: 20});
+    await page.type('#id-p',pass,{delay: 20});
     const buttonLogin = await page.$x('//button[div[text()=\'Увійти\']]');
     await buttonLogin[0].click({delay:20});
 
@@ -27,7 +30,7 @@ const puppeteer = require('puppeteer');
 
         await page.waitForXPath(emailField);
         const inputMail = await page.$x(emailField);
-        await inputMail[0].type(mailName,{delay: 5})
+        await inputMail[0].type(log,{delay: 5})
 
         await page.waitForXPath(emailTheme);
         const inputTheme = await page.$x(emailTheme);
@@ -62,6 +65,7 @@ const puppeteer = require('puppeteer');
         }
         return str.length;
     }
+
     const splitValue = new Array();
     const splitLeteral = new Array();
     for(let i = 0; i < allMessages.length;i++){
@@ -78,15 +82,15 @@ const puppeteer = require('puppeteer');
 
     await page.waitForXPath(buttonWriteLetter);
     const buttonWrite = await page.$x(buttonWriteLetter);
-    await buttonWrite[0].click({delay:5});
+    await buttonWrite[0].click({delay:25});
 
     await page.waitForXPath(emailField);
     const inputMail = await page.$x(emailField);
-    await inputMail[0].type(mailName,{delay: 5})
+    await inputMail[0].type(log,{delay:25})
 
     await page.waitForXPath(emailTheme);
     const inputTheme = await page.$x(emailTheme);
-    await inputTheme[0].type('Last one',{delay:5});
+    await inputTheme[0].type('Last one',{delay:25});
 
     let iter = 0;
     for (let key of messageMap.keys()) {
@@ -97,22 +101,30 @@ const puppeteer = require('puppeteer');
         await frame.waitForSelector('#tinymce');
 
         const emailBody = await frame.$('#tinymce');
-        await emailBody.type(`"Received mail on theme ${key} with message: ${messageMap.get(key)}. It contains ${splitLeteral[iter]} letters and ${splitValue[iter]} numbers"\n`,{delay:5});
+        await emailBody.type(`"Received mail on theme ${key} with message: ${messageMap.get(key)}. It contains ${splitLeteral[iter]} letters and ${splitValue[iter]} numbers"\n`,{delay:15});
 
         iter++;
     }
-
+    await page.waitForXPath('//button[text()=\'Надіслати\']');
     const buttonSend = await page.$x('//button[text()=\'Надіслати\']');
     await buttonSend[0].click({delay: 20});
 
     await page.$x('//span[text()=\'Вхідні\']');
     await buttonInEmail[0].click({delay:20});
 
+    await page.waitForXPath('//div[@class =\'msglist__checkbox\']/label/span',{visible:true});
     const selectAllLetters = await page.$x('//div[@class =\'msglist__checkbox\']/label/span');
     await selectAllLetters[0].click({delay: 20});
 
+    await page.waitForXPath('//div[@class = \'controls controls_msglist\']/div/a[text()=\'Видалити\']',{visible:true});
     const deleteLetters = await page.$x('//div[@class = \'controls controls_msglist\']/div/a[text()=\'Видалити\']');
     await deleteLetters[0].click({delay: 20});
 
    //await browser.close();
 })();
+}
+Promise.all([
+    test(config.login1[0],config.login1[1]),
+    test(config.login2[0],config.login2[1]),
+    test(config.login3[0],config.login3[1])
+])
